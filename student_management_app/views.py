736622a6e3318a9219b58b482a5f4a1b58a8ff7a
Brightnyandiko <carlsonbright98@gmail.com>
2024-02-27 16:42:13 +0300

    changed superuser login credentials

--- student_management_app/views.py
+++ student_management_app/views.py
@@ -18,8 +18,9 @@ def doLogin(request):
     if request.method != "POST":
         return HttpResponse("<h2>Method Not Allowed</h2>")
     else:
-        user = EmailBackEnd.authenticate(request, username=request.POST.get("email"), password=request.POST.get("password"))
-        if user is not None:
+        user = EmailBackEnd.authenticate(request, username=request.POST.get("email"),
+                                         password=request.POST.get("password"))
+        if user != None:
             login(request, user)
             return HttpResponse("Email : " + request.POST.get("email") + " Password : " + request.POST.get("password"))
         else:
@@ -27,7 +28,7 @@ def doLogin(request):
 
 
 def GetUserDetails(request):
-    if request is not None:
+    if request != None:
         return HttpResponse("User : " + request.user.email + " usertype : " + request.user.user_type)
     else:
         return HttpResponse("Please Login First")
