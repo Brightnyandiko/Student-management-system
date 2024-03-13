270f3633f9913273ea63124069ae529347140afe
salvator-del <65698466+salvator-del@users.noreply.github.com>
2024-03-13 13:19:20 +0000

    Added default values

--- student_management_app/HodViews.py
+++ student_management_app/HodViews.py
@@ -24,7 +24,7 @@ def add_staff_save(request):
         password = request.POST.get("password")
         address = request.POST.get("address")
         # try:
-        user = CustomUser.objects.create_user(username=username, password=password, email=email,last_name=last_name, first_name=first_name, user_type=2)
+        user = CustomUser.objects.create_user(username=username, password=password, email=email, last_name=last_name, first_name=first_name, user_type=2)
         user.staffs.address = address
         user.save()
         messages.success(request,"successfully Added Staff")
