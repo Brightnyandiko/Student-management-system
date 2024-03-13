96698f98bc2d46e3191c431d19baf553f3ddaa8c
Nathaniel Gachukia Gachago <140628508+teegash@users.noreply.github.com>
2024-03-13 12:03:05 +0300

    Update HodViews.py
    Indentation change in line CustomUser create

--- student_management_app/HodViews.py
+++ student_management_app/HodViews.py
@@ -24,11 +24,10 @@ def add_staff_save(request):
         password = request.POST.get("password")
         address = request.POST.get("address")
         # try:
-        user = CustomUser.objects.create_user(username=username, password=password, email=email,
-                                              last_name=last_name, first_name=first_name, user_type=2)
+        user = CustomUser.objects.create_user(username=username, password=password, email=email,last_name=last_name, first_name=first_name, user_type=2)
         user.staffs.address = address
         user.save()
-        messages.success(request, "successfully Added Staff")
+        messages.success(request,"successfully Added Staff")
         return HttpResponseRedirect("/add_staff")
         # except:
         #     messages.error(request, "Failed to Add Staff")
