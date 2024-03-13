b0b7f2ff5089101d0b3811e063108f9b5d6fad2d
Nathaniel Gachukia Gachago <140628508+teegash@users.noreply.github.com>
2024-03-13 12:53:07 +0300

    Update HodViews.py
    comment removed

--- student_management_app/HodViews.py
+++ student_management_app/HodViews.py
@@ -23,12 +23,12 @@ def add_staff_save(request):
         email = request.POST.get("email")
         password = request.POST.get("password")
         address = request.POST.get("address")
-        # try:
-        user = CustomUser.objects.create_user(username=username, password=password, email=email,last_name=last_name, first_name=first_name, user_type=2)
-        user.staffs.address = address
-        user.save()
-        messages.success(request,"successfully Added Staff")
-        return HttpResponseRedirect("/add_staff")
-        # except:
-        #     messages.error(request, "Failed to Add Staff")
-        #     return HttpResponseRedirect("/add_staff")
+        try:
+            user=CustomUser.objects.create_user(username=username, password=password, email=email,last_name=last_name, first_name=first_name, user_type=2)
+            user.staffs.address = address
+            user.save()
+            messages.success(request,"successfully Added Staff")
+            return HttpResponseRedirect("/add_staff")
+        except:
+            messages.error(request, "Failed to Add Staff")
+            return HttpResponseRedirect("/add_staff")
